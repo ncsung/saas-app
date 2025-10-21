1c79b202a2c2fd593db14522e3da344ee595e4a6
Nick Sung <nick_sung@sfu.ca>
2025-10-21 11:43:33 -0700

    implement home companions cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import Cta from '@/components/Cta'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <>
-      <h1 className="text-2xl underline"> Welcome to my SaaS App</h1>
-      <Button>Let's get started</Button>
-    </>
+      <main>
+        <h1 className="text-2xl underline"> Popular Companions</h1>
+        <section className='home-section'>
+          <CompanionCard 
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="Science"
+            duration={45}
+            color="#ffda6e"
+          />
+          <CompanionCard 
+            id="456"
+            name="Countsy the Number Wizard"
+            topic="Derivatives & Integrals"
+            subject="Maths"
+            duration={30}
+            color="#BDE7FF"
+          />
+          <CompanionCard 
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="English Literature"
+            subject="Language"
+            duration={35}
+            color="#e5d0ff"
+          />
+        </section>
+        <section className='home-section'>
+          <CompanionsList/>
+          <Cta/>
+        </section>
+      </main>
   )
 }
 
